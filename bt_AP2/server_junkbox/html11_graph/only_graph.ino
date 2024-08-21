661cb147daa741e2eba514ea05a6217f1b2b6c7a
piotrcurious <78758456+piotrcurious@users.noreply.github.com>
2024-08-21 17:59:35 +0200

    Create only_graph.ino

---
+++ bt_AP2/server_junkbox/html11_graph/only_graph.ino
@@ -0,0 +1,71 @@
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+
+// Wi-Fi credentials
+const char* ssid = "your_SSID";
+const char* password = "your_PASSWORD";
+
+// Create an instance of the server
+AsyncWebServer server(80);
+
+// Define the array of floats
+float dataArray[] = {1.0, 2.5, 3.5, 4.0, 5.2, 3.8, 2.2, 1.1};
+int dataArraySize = sizeof(dataArray) / sizeof(dataArray[0]);
+
+// Define graph settings
+float yMin = 0.0;
+float yMax = 6.0;
+int barResolution = 10; // Number of '0' characters per unit of float value
+int fontSize = 12; // Font size in pixels
+
+// Function to generate ASCII art bars
+String generateAsciiGraph() {
+  String html = "<html><body><table style='width:100%;font-size:" + String(fontSize) + "px;'>";
+
+  // Determine the dynamic height scale
+  float yRange = yMax - yMin;
+
+  for (int i = 0; i < dataArraySize; i++) {
+    // Map the float value to the number of '0' characters
+    int barHeight = (dataArray[i] - yMin) / yRange * barResolution;
+
+    // Generate the table column with the ASCII art bar
+    html += "<td style='vertical-align:bottom;text-align:center;'>";
+    for (int j = 0; j < barHeight; j++) {
+      html += "0<br>";
+    }
+    html += "</td>";
+  }
+
+  html += "</table></body></html>";
+  return html;
+}
+
+// Handle root URL request
+void handleRequest(AsyncWebServerRequest *request) {
+  String html = generateAsciiGraph();
+  request->send(200, "text/html", html);
+}
+
+void setup() {
+  // Start Serial communication
+  Serial.begin(115200);
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  // Start the server and define the root URL handling
+  server.on("/", HTTP_GET, handleRequest);
+
+  // Start the server
+  server.begin();
+}
+
+void loop() {
+  // Nothing to do here, everything is handled asynchronously
+}
