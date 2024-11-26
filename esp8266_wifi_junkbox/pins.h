e372b48fad1bcd0f96efca208c1488336c2ab60b
piotrcurious <78758456+piotrcurious@users.noreply.github.com>
2024-11-26 18:37:16 +0000

    Create pins.h

---
+++ esp8266_wifi_junkbox/pins.h
@@ -0,0 +1,26 @@
+// pins.h
+
+#define PIN_3V3    -1
+#define PIN_D0     16
+#define PIN_D1      5
+#define PIN_D2      4
+#define PIN_D3      0
+#define PIN_D4      2
+#define PIN_D5     14
+#define PIN_GND    -1
+#define PIN_RST    -1
+#define PIN_A0     A0
+#define PIN_G      -1
+#define PIN_5V     -1
+#define PIN_D8     15
+#define PIN_D7     13
+#define PIN_D6     12
+#define PIN_3V3B   -1
+
+#define EPAPER_CS       PIN_D8
+#define EPAPER_DC       PIN_D3
+#define EPAPER_RESET    PIN_D4
+#define EPAPER_BUSY     PIN_D2
+#define EPAPER_SCLK     PIN_D5
+#define EPAPER_MOSI     PIN_D7
+#define EPAPER_MISO     PIN_D6
